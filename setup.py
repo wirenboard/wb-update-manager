7a10050029c6902775d13525bf63c32658bcc53d
Nikita Maslov <nikita.maslov@wirenboard.ru>
2021-03-25 17:43:00 +0300

    32254: initial commit for wb-releases

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name="wb-update-manager",
+      version="1.0",
+      description="Wirenboard software updates and release management tool",
+      author="Nikita Maslov",
+      author_email="nikita.maslov@wirenboard.ru",
+      packages=['wb.update_manager']
+     )
