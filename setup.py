c1a8fae52ea3c99d8bbe2d8be939b64add6ac469
Nikolay Korotkiy <sikmir@disroot.org>
2023-07-25 11:12:15 +0400

    Fix pkg-info, add LICENSE (#36)

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@
 
 from setuptools import setup
 
+
+def get_version():
+    with open("debian/changelog", "r", encoding="utf-8") as f:
+        return f.readline().split()[1][1:-1]
+
+
 setup(
     name="wb-update-manager",
-    version="1.0",
+    version=get_version(),
     description="Wirenboard software updates and release management tool",
+    license="MIT",
     author="Nikita Maslov",
     author_email="nikita.maslov@wirenboard.ru",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
+    url="https://github.com/wirenboard/wb-update-manager",
     packages=["wb.update_manager"],
 )
